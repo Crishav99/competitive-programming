6bc68bcc45d355884d98e8fbe514b831e755bd90
Crishav99 <crishav55@gmail.com>
2026-07-30 23:22:49 +0530

    Add files via upload

---
+++ 122A.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class 122A {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] lucky = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+
+        for (int num : lucky) {
+            if (n % num == 0) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
+}
