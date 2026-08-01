f3e670f401b84d6a91e381cf1c272f99c61d45ce
Crishav99 <crishav55@gmail.com>
2026-08-01 23:26:23 +0530

    Add files via upload

---
+++ 443A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class 443A {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                set.add(ch);
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
+
