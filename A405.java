9ed500f979113d8805d7fb9789aea2b17235597c
Crishav99 <crishav55@gmail.com>
2026-08-06 23:24:04 +0530

    Add files via upload

---
+++ A405.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+ public class A405 {
+   
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        Arrays.sort(a);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        sc.close();
+    }
+}
+
